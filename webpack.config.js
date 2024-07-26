0e671b6c10bf00136a997287932b8484c263885e
huhongxia <huhongxia@baidu.com>
2024-07-26 14:32:29 +0800

    环境变量

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         // 生成index.html文件并自动引入bundle.js文件
         // 向 HTML 动态添加 bundle
         new HtmlWebpackPlugin({
-            title:'caching'
+            title:'构建性能'
         }),
     ],
     output:{
@@ -18,29 +18,14 @@ module.exports = {
         // 清除dist文件夹中不要的旧文件
         clean:true
     },
-    optimization: {
-        // 所有 chunk 创建一个 runtime bundle
-        runtimeChunk: 'single',
-        splitChunks:{
-            // 控制多次构建，vendor 的哈希值都应保持一致
-            moduleIds:"deterministic",
-            // 由于像 lodash 或 react 这样的第三方库很少像本地源代码一样频繁修改，
-            // 因此通常推荐将第三方库提取到单独的 vendor chunk 中。
-            // 这一步将减少客户端对服务器的请求，同时保证自身代码与服务器一致。
-            cacheGroups:{
-                vendor:{
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
-                }
-            }
-        }
-    },
     module:{
         rules:[
             {
-                test:/\.js$/,
-                loader:'babel-loader'
+                // 将 loader 应用于最少数量的必要模块
+                // 使用 include 字段将 loader 应用在实际需要将其转换的模块
+                test: /\.js$/,
+                include: path.resolve(__dirname,'src'),
+                loader: 'babel-loader'
             }
         ]
     }
